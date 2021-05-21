7a3f73f15f99651967cc0912a3aa34dd83154c03
henry-sev <q2466351926@gmail.com>
2021-05-21 19:45:17 +0800

    fix bug: Some books don't have image links for content

--- src/Book.js
+++ src/Book.js
@@ -12,7 +12,12 @@ class Book extends Component {
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+            {
+              book.imageLinks 
+              ? (<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>)
+              : (<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: '' }}>No Content</div>)
+            }
+            
             <div className="book-shelf-changer">
               <select onChange={this.handleUpdateShelf}>
                 <option value="move" disabled>Move to...</option>
